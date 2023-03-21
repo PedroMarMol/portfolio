256bdb555259ca2916dd4ed80dd87f23f0d081eb
PedroMarMol <martosmoleropedro@gmail.com>
2023-03-21 11:18:12 +0100

    Create components and add scroll effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import { Contact } from "./components/Contact"
+import { Info } from "./components/Info"
+import { Projects } from "./components/Projects"
+import { User } from "./components/User"
+import styled from "styled-components"
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url('./img/1268183.jpg');
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`
+
 function App() {
 
   return (
     <div>
-      Hello world!
+      <Container>
+        <User />
+        <Info />
+        <Projects />
+        <Contact />
+      </Container>
     </div>
   )
 }
