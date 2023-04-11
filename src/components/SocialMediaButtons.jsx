5f6b638bdb730c012f0255a0bf9553a2b20cda65
PedroMarMol <martosmoleropedro@gmail.com>
2023-04-11 19:41:41 +0200

    Create component with linkedin/twitter/github buttons and links to my pages

---
+++ src/components/SocialMediaButtons.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import IconButton from '@material-ui/core/IconButton'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+
+export default function SocialMediaButtons() {
+  const githubUrl = "https://github.com/pedromarmol"
+  const linkedinUrl = "https://linkedin.com/in/pedrommartos"
+  const twitterUrl = "https://twitter.com/pedromartos_dev"
+  
+  return (
+   <div>
+      <a href={twitterUrl}>
+        <IconButton>
+          <TwitterIcon />
+        </IconButton>
+      </a>
+      <a href={githubUrl}>
+        <IconButton>
+          <GitHubIcon />
+        </IconButton>
+      </a>
+      <a href={linkedinUrl}>
+        <IconButton>
+          <LinkedInIcon />
+        </IconButton>
+      </a>
+    </div>
+  )
+}
